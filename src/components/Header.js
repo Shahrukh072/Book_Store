bd333fe23013d7a93f9bf68933ec354bc617cefb
shahrukh khan <shahrukh.khan072@gmail.com>
2023-05-24 21:07:20 +0530

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,16 @@ const Header = () => {
     <header className="header">
       <div className="container mx-auto header-container">
         <img
-          src="/logo.png"
+          src="logo.png"
           alt="Logo"
           className="header-logo"
         />
          
         <nav className="header-nav">
           <a href="Home">Home</a>
-          <a href="BookCategories">BookCategories</a>
-          <a href="FeaturedBooks">FeaturedBooks</a>
-          <a href="RecentlyAddedBooks">RecentlyAddedBooks</a>
+          <a href="Books">Books</a>
+          <a href="Authors">Authors</a>
+          <a href="Categories">Categories</a>
           <a href="About">About</a>
         </nav>
         <SearchBar />
